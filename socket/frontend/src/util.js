32173bbf8387f9fd4cde8551df4ee9ac40a58e63
steven_hert <108745586+steven3002@users.noreply.github.com>
2024-03-26 11:02:13 +0100

    commit

--- socket/frontend/src/util.js
+++ socket/frontend/src/util.js
@@ -46,6 +46,112 @@ export const waitForTransaction = async (tx, toast)=>{
     return receipt
 }
 
+import {NONCE_KEY, MOVE_KEY, INPUTBOX_ADDRESS, DAPP_ADDRESS, DEFAULT_URL} from "./constant";
+import {InputBox__factory} from "@cartesi/rollups"
+import {ethers} from "ethers";
+
+
+
+
+
+export const sendInput =async(value, signer, toast)=>{
+    const inputBox = InputBox__factory.connect(INPUTBOX_ADDRESS, signer)
+    const inputBytes = ethers.isBytesLike(value)
+
+        ? value
+        : ethers.toUtf8Bytes(value)
+    const tx = await inputBox.addInput(DAPP_ADDRESS, inputBytes)
+    return await waitForTransaction(tx, toast)
+}
+
+export const waitForTransaction = async (tx, toast)=>{
+    toast({
+        title:"Transaction sent",
+        description: "waiting for confirmation",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+        position: "top-left",
+    })
+    const receipt = await tx.wait(1)
+    let event = receipt.events?.find((e)=>e.event === "InputAdded")
+        toast({
+        title:"confirmed",
+        description: ``,
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+        position: "top-left",
+    })
+
+    return receipt
+}
+
+
+export const inspect = async (payload) => {
+    try {
+        const url = `${DEFAULT_URL}/${encodeURIComponent(JSON.stringify(payload))}`;
+        const response = await fetch(url);
+
+        if (response.ok) {
+            const result = await response.json();
+            console.log(result)
+            const hex_code = result.reports[0].payload
+
+            return hex2str(hex_code);
+        } else {
+            console.error(`Failed to fetch data. Status: ${response.status}`);
+            // Handle other status codes if needed
+            return null;
+        }
+    } catch (error) {
+        console.error('An error occurred:', error);
+        return null;
+    }
+}
+
+
+
+
+export
+
+// Function to fetch report from the backend
+function fetchReport() {
+  fetch(`${DEFAULT_URL}/finish`)
+  .then(response => {
+    console.log(`Received report status ${response.status}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Assuming the response is in JSON format
+  })
+  .then(data => {
+    console.log('Received report data:', data);
+    // Handle the received report data as needed
+    // Assuming the data follows the Report schema you provided
+    const { index, input, payload } = data;
+    console.log('Report Index:', index);
+    console.log('Input:', input);
+    console.log('Payload:', payload);
+  })
+  .catch(error => {
+    console.error('Error fetching report:', error);
+  });
+}
+
+
+
+
+
+
+
+export const hex2str = (hex) => {
+    try {
+        return ethers.toUtf8String(hex);
+    } catch (e) {
+        return hex;
+    }
+}
 
 export const inspect = async (payload) => {
     try {
